5b4a5af875e28be5debe552e51a03a24a6bd83b9
shane new <shanenew13@gmail.com>
2013-05-07 17:15:53 -0500

    add pub lunch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
-add_lunch_combo aokp_mako-userdebug
-add_lunch_combo aokp_maguro-userdebug
-add_lunch_combo aokp_toro-userdebug
+add_lunch_combo aokpub_mako-userdebug
+add_lunch_combo aokpub_maguro-userdebug
+add_lunch_combo aokpub_toro-userdebug
 add_lunch_combo aokp_toroplus-userdebug
 add_lunch_combo aokp_grouper-userdebug
 add_lunch_combo aokp_tilapia-userdebug
